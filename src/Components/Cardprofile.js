62aa9a21d2f4233f5013969036cb781fad4107cb
Irfnmdlptra <bangasrat@gmail.com>
2023-05-24 10:03:54 +0800

    4

--- src/Components/Cardprofile.js
+++ src/Components/Cardprofile.js
@@ -1,25 +1,28 @@
 import React from "react";
+import "../css/Style.css";
 
-const Cardprofile = ()=> {
-    return (
-        // 1 inline css
-        <div style={{
-            background: 'aqua', 
-            width: '100%', 
-            height: '100vh',
-            display : "flex",
-            justifyContent : 'center',
-            alignItems : 'center',
-            }}>
-            <div className="card">
-                <img />
-                <div>Frontend Developer</div>
-                <h2>Irfan Syahputra</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque beatae minima veniam libero at! Fugit ipsum quos inventore excepturi aspernatur.</p>
-                <button>Contact Me</button>
-            </div>
-        </div>
-    )
-}
+const Cardprofile = () => {
+  return (
+    // 1 inline css
+    <div
+      style={{
+        background: "aqua",
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="card">
+        <img />
+        <div>Frontend Developer</div>
+        <h2>Irfan Syahputra</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque beatae minima veniam libero at! Fugit ipsum quos inventore excepturi aspernatur.</p>
+        <button>Contact Me</button>
+      </div>
+    </div>
+  );
+};
 
-export default Cardprofile
+export default Cardprofile;
