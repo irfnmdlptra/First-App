447484f34e1310620ec6e5c1ef09179827671c43
Irfnmdlptra <bangasrat@gmail.com>
2023-05-24 09:06:50 +0800

    card

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ButtonJsx from './Components/Components';
+import Cardprofile from './components/Cardprofile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ButtonJsx />
+    < Cardprofile/>
   </React.StrictMode>
 );
 
