9992aa06814b0586992cea999bd6c5d547d9bd52
Irfnmdlptra <bangasrat@gmail.com>
2023-05-24 09:11:18 +0800

    2

--- src/Components/Cardprofile.js
+++ src/Components/Cardprofile.js
@@ -3,7 +3,13 @@ import React from "react";
 const Cardprofile = ()=> {
     return (
         <div>
-            <div></div>
+            <div>
+                <img />
+                <div>Frontend Developer</div>
+                <h2>Irfan Syahputra</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque beatae minima veniam libero at! Fugit ipsum quos inventore excepturi aspernatur.</p>
+                <button>Contact Me</button>
+            </div>
         </div>
     )
 }
