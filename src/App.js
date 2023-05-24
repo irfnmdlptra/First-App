f05d5b9171ab3d76dc929a1daca5bfc84f4107e4
Irfnmdlptra <bangasrat@gmail.com>
2023-05-24 13:54:25 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
@@ -6,11 +5,13 @@ class App extends React.Component{
   constructor(props) {
     super(props);
 
-
+    // Array adalah struktur data untuk menyimpan sebuah data di dalam memory komputer dalam satu tempat. data yang didalam Array harus di apit denagn tanda kutip atas dan untuk memisahkannya menggunakan tanda koma 
     const fruit1 = 'mangga';
     const fruit2 = 'apple';
     const fruit3 = 'delima';
     const fruits = ['mangga', 'apple', 'delima', 'nanas']
+
+    alert(fruit1);
   }
 
   render() {
