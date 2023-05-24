a30fa5cf90537aebb09f151701922e881e8bb4bc
Irfnmdlptra <bangasrat@gmail.com>
2023-05-24 09:01:05 +0800

    compt

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,28 +1,60 @@
 import React from 'react'
 
+// Props Adalah Singkatan Dari Singkatan Dari Properti Seperti halnya dengan function di dalam javascript yang menerima improf
 
 // 1. Class Component
 class ButtonJsx extends React.Component {
     render() {
-        return <button className='btn btn-primary'>Tombol</button>
+        return <button className='btn btn-warning'>{this.props.text}</button>
     }
 }
-// 2. Function Component 
-function Card() {
-    const Btn = () => {
-        return ButtonJsx
+const Button2 = () => {
+    return <button  className='btn btn-danger m-5'>Btn Danger</button>
+}
+
+// Bagian Card 
+class CardJsx extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            Judul : "Aku ADALAH STATE",
+            Title : "Aku ADALAH SUB TITLE CARD"
+        }
     }
-    return (
-        Btn,
-        <div className="card">
-            <div className="card-body">
-                <h2 className="card-title">Saya Card</h2>
-                <h5 className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, maxime!</h5>
-                <a href="#" className="btn btn-danger">Click</a>
+
+    UbahJudul = () => {
+        this.setState({
+            Judul : " Aku sudah berubah jadi setState"
+            
+        })
+    }
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.Judul}</h5>
+                    <p className="card-text">{this.state.Title} </p>
+                    <ButtonJsx text="Ini Adalah Tombol"/>
+                    <button onClick={this.UbahJudul} className='btn btn-danger m-5'>Btn Danger</button>
+                </div>
             </div>
-        </div>
+        )
+    }
+}
+
+const CardFunction = () => {
+    const [state, setState] = React.useState('Judul Card Menggunakan React HOOKS');
+    return (
+        <>
+        <h1>{state}</h1>
+
+        <CardJsx/>
+        <Button2/>
+        </>
 
     )
-    }
+}
 
-export default Card
+
+export default CardFunction
