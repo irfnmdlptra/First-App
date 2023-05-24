3728689066356098a7f8384fa0c59dca8909b0ec
Irfnmdlptra <bangasrat@gmail.com>
2023-05-25 07:50:07 +0800

    d

--- src/Components/Object.js
+++ src/Components/Object.js
@@ -2,5 +2,21 @@
 
 
 const Motor = {
-    merek : 'Honda '
+    merek : 'Honda HDV 160',//Key -- Value
+    jenis : 'Matic',
+    Warna : 'Merah',//Property
+    bensin : 10,
+    harga : 30000000,
+    plat : "DD 1945 UD",
+    status : 'OFF',
+
+    nyalakan : ()=> {
+        setMotor( data => {
+            return {
+                ...data,
+                status : 'ON',
+                bensin : data.bensin - 5
+            }
+        })
+    }
 }
