9531e78d74403df91981269296953c3d808ce04e
Irfnmdlptra <bangasrat@gmail.com>
2023-05-24 13:41:41 +0800

    3

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class List extends React.Component{
+    constructor(props) {
+        super(props);
+
+        const fruit1 = 'mangga';
+        const fruit2 = 'apple';
+        const fruit3 = 'delima';
+        const fruits = ['mangga','apple','delima', 'nanas']
+    }
     render() {
         return (
             <>
