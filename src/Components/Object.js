8f092e50bf67db8f90869b15a91442efa56d702b
Irfnmdlptra <bangasrat@gmail.com>
2023-05-25 09:19:01 +0800

    s

--- src/Components/Object.js
+++ src/Components/Object.js
@@ -1,49 +1,45 @@
 import { useState } from "react";
 
-const Object = ()=> {
+const Object = () => {
+  let [mtr, setMtr] = useState({
+    merek: "YAMAH", //Key -- Value
+    jenis: " JUPITER MX 135",
+    warna: "Merah", //Property
+    bensin: 10,
+    harga: 25000000,
+    plat: "DD 1945 UD",
+    status: "OFF",
 
-    let [mtr, setMtr] = useState(
-        {
-            merek : 'YAMAH',//Key -- Value
-            jenis : ' JUPITER MX 135',
-            warna : 'Merah',//Property
-            bensin : 10,
-            harga : 25000000,
-            plat : "DD 1945 UD",
-            status : 'OFF',
-        
-            // 2. Metod 
-            nyalakan : ()=> {
-                console.log('nyalakan mesin');
-                setMtr( {status : 'ON'} )
-                console.log(motor.status)
-            }
-        }
-    )
+    // 2. Metod
+    nyalakan: () => {
+      console.log("nyalakan mesin");
+      setMtr({ status: "ON" });
+      console.log(motor.status);
+    },
+  });
 
-
-const motor = {
+  const motor = {
     // 1. Property
-    merek : 'YAMAH',//Key -- Value
-    jenis : ' JUPITER MX 135',
-    warna : 'Merah',//Property
-    bensin : 10,
-    harga : 25000000,
-    plat : "DD 1945 UD",
-    status : 'OFF',
+    merek: "YAMAH", //Key -- Value
+    jenis: " JUPITER MX 135",
+    warna: "Merah", //Property
+    bensin: 10,
+    harga: 25000000,
+    plat: "DD 1945 UD",
+    status: "OFF",
 
-    // 2. Metod 
-    nyalakan : ()=> {
-        console.log('nyalakan mesin');
-        
-        motor.status = 'ON';
-        console.log(mtr)
-    }
-}
-return (
+    // 2. Metod
+    nyalakan: () => {
+      console.log("nyalakan mesin");
+
+      motor.status = "ON";
+      console.log(mtr);
+    },
+  };
+  return (
     <>
-    <h1>Motor</h1>
-    <ul>
+      <h1>Motor</h1>
+      <ul>
         <li>Merek : {mtr.merek}</li>
         <li>Jenis : {mtr.jenis} </li>
         <li>warna : {mtr.warna} </li>
@@ -51,10 +47,12 @@ return (
         <li>harga : {mtr.harga} </li>
         <li>plat : {mtr.plat} </li>
         <li>status : {mtr.status} </li>
-    </ul>
-    <button style={{padding:'10px 20px', background:'aqua',color:'blue',borderRadius:'10px',fontFamily:'arial'}} onClick={()=> mtr.nyalakan()}>Nyalakan</button>
+      </ul>
+      <button style={{ padding: "10px 20px", background: "aqua", color: "blue", borderRadius: "10px", fontFamily: "arial" }} onClick={() => mtr.nyalakan()}>
+        Nyalakan
+      </button>
     </>
-)
-}
+  );
+};
 
-export default Object
+export default Object;
