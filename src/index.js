962ddbc665f30d70e4bfec44a444f26a792df703
Irfnmdlptra <bangasrat@gmail.com>
2023-05-25 08:24:45 +0800

    s

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Cardprofile from './components/Cardprofile';
+import Object from './components/Object';
+// import Cardprofile from './components/Cardprofile';
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    < Cardprofile/>
+    <Object/>
   </React.StrictMode>
 );
 
