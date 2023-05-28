99c7038074b7ba321835fb438768a63c4efbfe81
Irfnmdlptra <bangasrat@gmail.com>
2023-05-28 08:28:30 +0800

    12

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -4,6 +4,8 @@ class Event extends React.Component {
     render() {
         return(
             <>
+
+            <button className="btn btn-outline-primary" onClick={()=> alert('gasss')}>click me</button>
             </>
         )
     }
