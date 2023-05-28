204cc5bdb2e793f2964922c0695e2327d0fc5d26
Irfnmdlptra <bangasrat@gmail.com>
2023-05-28 08:36:56 +0800

    13

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,11 +1,14 @@
 import React from "react";
-
 class Event extends React.Component {
+    kirimPesan(nama) {
+        alert('hallo nama aku ' + nama)
+    }
     render() {
         return(
             <>
 
             <button className="btn btn-outline-primary" onClick={()=> alert('gasss')}>click me</button>
+            <button className="btn btn-btn-outline-secondary" onClick={()=> this.kirimPesan.bind('agus')}>click me</button>
             </>
         )
     }
