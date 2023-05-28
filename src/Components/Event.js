4df59a21decea565a06abbb156c45d418ad28471
Irfnmdlptra <bangasrat@gmail.com>
2023-05-28 08:47:13 +0800

    16

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -3,12 +3,17 @@ class Event extends React.Component {
     kirimPesan(nama) {
         alert('hallo nama aku ' + nama)
     }
+
+    ubahTulisan() {
+        e.target.innerHTML= 'KIRIM PESAN'
+    }
     render() {
         return(
             <>
 
             <button className="btn btn-outline-primary" onClick={(e)=> this.kirimPesan('gasss', e)}>click me</button>
-            <button className="btn btn-btn-outline-secondary" onClick={ this.kirimPesan.bind(this, 'agus')}>click me</button>
+            <button className="btn -btn-outline-secondary" onClick={ this.kirimPesan.bind(this, 'agus')}>click me</button>
+            <button className="btn btn-outline-primary" onMouseEnter={(e)=> this.ubahTulisan('eeee',e)}>click me</button>
             </>
         )
     }
