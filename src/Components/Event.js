056ee28d8b14984360546a2dd65d12b8a58aa9de
Irfnmdlptra <bangasrat@gmail.com>
2023-05-28 08:50:20 +0800

    19

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -4,10 +4,10 @@ class Event extends React.Component {
         alert('hallo nama aku ' + nama)
     }
 
-    ubahTulisan() {
+    ubahTulisan(e) {
         e.target.innerHTML= 'Kirim Pesan'
     }
-    ubahLagi() {
+    ubahLagi(e) {
         e.target.innerHTML = 'click me'
     }
     render() {
@@ -17,7 +17,7 @@ class Event extends React.Component {
             <button className="btn btn-outline-primary" onClick={(e)=> this.kirimPesan('gasss', e)}>click me</button>
             <button className="btn -btn-outline-secondary" onClick={ this.kirimPesan.bind(this, 'agus')}>click me</button>
             <button className="btn btn-outline-primary" onMouseEnter={(e)=> this.ubahTulisan(e)}>click me</button>
-            <button className="btn btn-outline-primary" onMouseOut={(e)=> this.ubahTulisan(e)}>click me</button>
+            <button className="btn btn-outline-primary" onMouseOut={(e)=> this.ubahLagi(e)}>click me</button>
             </>
         )
     }
