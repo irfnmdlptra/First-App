94b547a499e3be94eeb8e81fb8beeb54323fd65b
Irfnmdlptra <bangasrat@gmail.com>
2023-05-28 08:49:38 +0800

    18

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -17,6 +17,7 @@ class Event extends React.Component {
             <button className="btn btn-outline-primary" onClick={(e)=> this.kirimPesan('gasss', e)}>click me</button>
             <button className="btn -btn-outline-secondary" onClick={ this.kirimPesan.bind(this, 'agus')}>click me</button>
             <button className="btn btn-outline-primary" onMouseEnter={(e)=> this.ubahTulisan(e)}>click me</button>
+            <button className="btn btn-outline-primary" onMouseOut={(e)=> this.ubahTulisan(e)}>click me</button>
             </>
         )
     }
